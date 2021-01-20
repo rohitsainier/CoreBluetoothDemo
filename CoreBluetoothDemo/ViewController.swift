6710118c21e4c1c81b8b9060b8ac08093d9d3223
rohitsainier <rohitsainier@gmail.com>
2021-01-20 13:39:56 +0530

    Initial Commit

---
+++ CoreBluetoothDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreBluetoothDemo
+//
+//  Created by Rohit Saini on 20/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
